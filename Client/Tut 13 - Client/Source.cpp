4ebace3eaaf6b5e2296c4a0638aeb42dbaff634d
elixirixile <jbocc524@gmail.com>
2018-04-25 03:27:45 -0400

    password asterisks

--- Client/Tut 13 - Client/Source.cpp
+++ Client/Tut 13 - Client/Source.cpp
@@ -5,23 +5,45 @@ Date: 02/26/2017
 */
 #include "Client.h"
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 bool isusername(string);
 bool ispassword(string);
+string getpass(const char *prompt, bool show_asterisk = true);
 
 int main()
 {
 	string userPass, usern;
+	int tries = 0;
+
 	cout << "Enter your username: " << endl;
 	cin >> usern;
 	if (isusername(usern)) {
-		cout << "Enter your password: " << endl;
-		cin >> userPass;
+		while (!ispassword(userPass) && tries < 3) {
+			userPass = getpass("Enter your password: ", true); // Show asterisks
+			if (ispassword(userPass))
+				cout << "Correct password" << endl;
+			else {
+				cout << "Incorrect password. Try again" << endl;
+				tries++;
+				if (tries == 3) {
+					userPass = getpass("Last try!: ", true); // Show asterisks
+					if (ispassword(userPass))
+						cout << "Correct password" << endl;
+					else {
+						cout << "Wrong password, exiting program." << endl;
+						return 0;
+					}
+				}
+			}			
+		}
+		
+
 	}
 
 	if (isusername(usern) && ispassword(userPass)) {
-		Client myClient("192.168.1.228",1111); //Create client to connect to server 127.0.0.1 [localhost] on port 1111
+		Client myClient("172.20.10.2",1111); //Create client to connect to server 127.0.0.1 [localhost] on port 1111
 			if (!myClient.Connect()) //If client fails to connect...
 			{
 				std::cout << "Failed to connect to server..." << std::endl;
@@ -59,4 +81,41 @@ bool ispassword(string password) {
 		return true;
 	else
 		return false;
+}
+
+string getpass(const char *prompt, bool show_asterisk)
+{
+	const char BACKSPACE = 8;
+	const char RETURN = 13;
+
+	string password;
+	unsigned char ch = 0;
+
+	cout << prompt << endl;
+
+	while ((ch = _getch()) != RETURN)
+	{
+		if (ch == BACKSPACE)
+		{
+			if (password.length() != 0)
+			{
+				if (show_asterisk)
+					cout << "\b \b";
+				password.resize(password.length() - 1);
+			}
+		}
+		else if (ch == 0 || ch == 224) // handle escape sequences
+		{
+			_getch(); // ignore non printable chars
+			continue;
+		}
+		else
+		{
+			password += ch;
+			if (show_asterisk)
+				cout << '*';
+		}
+	}
+	cout << endl;
+	return password;
 }
